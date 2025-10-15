ec9e97ba1606a6d37d3ec0ca7358f450b4690534
Ale Ivonne <aleivonne.ma@gmail.com>
2025-10-15 16:43:51 -0600

    2do select

--- src/ui/FormHousingData.jsx
+++ src/ui/FormHousingData.jsx
@@ -8,6 +8,31 @@ function FormHousingData() {
       <h2 className="text-lg font-semibold text-center mb-4 text-[#4b4b4b]">
         FORMULARIO DATOS DE VIVIENDA
       </h2>
+      
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="space-y-1">
+        <label className="block font-medium mb-1 font-poppins text-[#4b4b4b] ">
+              Tipo de Vivienda *
+            </label>
+             <select className="w-full border border-gray-300 rounded-md p-2 focus:ring focus:ring-[#A8C3A0] outline-none"
+              name="tipoVivienda">
+                <option value="" disabled selected></option>
+                <option value="1">Casa propia</option>
+                <option value="2">Casa rentada</option>
+                <option value="3">Departamento propio</option>
+                <option value="4">Departamento rentado</option>
+              </select>
+      </div>
+          
+        <label className="block font-medium mb-1 font-poppins text-[#4b4b4b] ">
+            <span className="text-sm text-gray-600">¿Tienes patio o terraza?</span>
+            <select className="w-full border border-gray-300 rounded-md p-2 focus:ring focus:ring-[#A8C3A0] outline-none"
+              name="patioTerraza">
+              <option></option>
+            </select>
+          </label>
+        
+      </div>
     </form>
   );
 }
